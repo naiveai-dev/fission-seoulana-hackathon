6254ab3cdb35e2a3214b993becf3ccf02ae548d3
mathiaskim522 <hjjam100@gmail.com>
2025-04-04 20:56:24 +0900

    feat: enhance Vite configuration with Tailwind CSS and Node.js polyfills

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,7 +1,52 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss(), nodePolyfills()],
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify',
+      'node-fetch': 'isomorphic-fetch',
+    },
+  },
+  define: {
+    process: {
+      env: {
+        NODE_DEBUG: false,
+      },
+      version: '',
+    },
+    global: {},
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis',
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+          process: true,
+        }),
+      ],
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        // Enable rollup polyfills plugin
+        // used during production bundling
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        rollupNodePolyFill(),
+      ],
+    },
+  },
+});
