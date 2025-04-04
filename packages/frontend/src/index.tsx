1e756968f708d8e1d44e709b90d5f42da4645d7a
mathiaskim522 <hjjam100@gmail.com>
2025-04-04 22:10:33 +0900

    feat: refactor layout for Mint Fission ID component in index.tsx

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -18,11 +18,10 @@ export default function Index() {
           <Box label="Wallet" className="col-span-3 sm:col-span-2">
             <Wallet />
           </Box>
-          <div className="col-span-3 sm:col-span-1">
-            <Box label="Mint Fission ID">
-              <Mint />
-            </Box>
-          </div>
+
+          <Box label="Mint Fission ID" className="col-span-3 sm:col-span-1">
+            <Mint />
+          </Box>
 
           <Box label="Quests" className="col-span-3 sm:col-span-2">
             <QuestSection />
